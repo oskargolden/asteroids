a69ec4fd80a4c1a48460093fe5f226548da3f2bb
Oskar Golden <oskar.golden@g.austincc.edu>
2024-10-04 12:27:18 -0500

    1: added basic sprite class.

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ from constants import *
 
 def main():
     pygame.init()
-    
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    # Create instance of pygame.time.Clock()
+    clock = pygame.time.Clock()
     while True:
-       for event in pygame.event.get():
-           if event.type == pygame.QUIT:
-               return
-       screen.fill((0,0,0))
-       pygame.display.flip()
-
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+                        
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")

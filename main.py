64701fed0701e05ce798f7ecab562f296251cfac
Oskar Golden <oskar.golden@g.austincc.edu>
2024-10-05 09:42:22 -0500

    drawing player triangle middle of screen

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 # this allows us to use code from
 # the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    # Create instance of pygame.time.Clock()
-    clock = pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-                        
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
-        main()
+    main()
